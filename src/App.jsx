5041cddfdae3ae353f72865a21e3003e90ffbe77
krishanathep@gmail.com <ap4imanee@gmail.com>
2023-08-02 16:48:08 +0700

    include react and map api

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MapComponent from "../src/components/map";
+
+function App() {
+  return (
+    <>
+      <MapComponent />
+    </>
+  );
+}
+
+export default App;
